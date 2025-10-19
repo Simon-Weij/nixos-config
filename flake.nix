5ab7cac4b08237e0bb3968385a35d3cacb98789a
Simon <175155691+Simon-Weij@users.noreply.github.com>
2025-10-19 13:21:03 +0200

    update

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     flatpaks.url = "github:gmodena/nix-flatpak/?ref=latest";
