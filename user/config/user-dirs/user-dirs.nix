94b4e5f75d09410bc817e731a0cc580f72ad98d7
Simon <simongvanderweij@gmail.com>
2025-12-28 19:46:44 +0100

    Fixed config

---
+++ user/config/user-dirs/user-dirs.nix
@@ -0,0 +1,9 @@
+{ flakeConfig }:
+{
+  ".config/user-dirs.dirs".text = ''
+    XDG_DOWNLOAD_DIR="$HOME/Downloads"
+    XDG_DOCUMENTS_DIR="$HOME/Documents"
+    XDG_PICTURES_DIR="$HOME/Pictures"
+    XDG_VIDEOS_DIR="$HOME/Videos"
+  '';
+}
--- user/packages/flatpak.nix
+++ user/packages/flatpak.nix
@@ -6,6 +6,7 @@
       "org.prismlauncher.PrismLauncher"
       "org.gnome.Boxes"
       "in.cinny.Cinny"
+      "com.dec05eba.gpu_screen_recorder"
     ];
   };
 }
