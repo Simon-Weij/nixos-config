120835c72dfab55d8c7ea7d1edc2549f71a503b5
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-01-03 14:34:45 +0100

    Got rid of useless packages

--- user/packages/flatpak.nix
+++ user/packages/flatpak.nix
@@ -2,10 +2,7 @@
   services.flatpak = {
     enable = true;
     packages = [
-      "org.vinegarhq.Sober"
       "org.prismlauncher.PrismLauncher"
-      "org.gnome.Boxes"
-      "in.cinny.Cinny"
       "com.dec05eba.gpu_screen_recorder"
     ];
   };
