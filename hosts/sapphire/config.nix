891e72ba34a2fdbe14c5e2baaa13e13c278ee5c1
Simon <simongvanderweij@gmail.com>
2026-02-05 10:26:40 +0100

    fractional scaling

--- hosts/sapphire/config.nix
+++ hosts/sapphire/config.nix
@@ -39,4 +39,17 @@ in {
       }
     ];
   };
+
+  systemd.user.services.gpu-screen-recorder = {
+    description = "GPU Screen Recorder UI";
+    wantedBy = ["graphical-session.target"];
+    after = ["graphical-session.target"];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.flatpak}/bin/flatpak run com.dec05eba.gpu_screen_recorder gsr-ui";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+  };
 }
--- system/systemd.nix
+++ system/systemd.nix
@@ -1,14 +1,2 @@
 {pkgs, ...}: {
-  systemd.user.services.gpu-screen-recorder = {
-    description = "GPU Screen Recorder UI";
-    wantedBy = ["graphical-session.target"];
-    after = ["graphical-session.target"];
-
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.flatpak}/bin/flatpak run com.dec05eba.gpu_screen_recorder gsr-ui";
-      Restart = "on-failure";
-      RestartSec = 5;
-    };
-  };
 }
