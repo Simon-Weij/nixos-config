e0ba83feb3c29c641318595d47b08b64db868159
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-02-10 20:33:26 +0100

    Added completion

--- user/config/bash/bash.nix
+++ user/config/bash/bash.nix
@@ -4,16 +4,20 @@
   inputs,
   ...
 }: {
-  programs.bash.promptInit = ''
-    alias config='code ${flakeConfig.flakePath}'
-    alias dockernuke='docker system prune -a -f --volumes'
-    alias dockerstop='docker stop $(docker ps -q)'
-    alias docs='cd ~/Documents'
+  programs.bash = {
+    promptInit = ''
+      alias config='code ${flakeConfig.flakePath}'
+      alias dockernuke='docker system prune -a -f --volumes'
+      alias dockerstop='docker stop $(docker ps -q)'
+      alias docs='cd ~/Documents'
 
-    export NH_FLAKE="${flakeConfig.flakePath}#${flakeConfig.networking.hostName}"
+      export NH_FLAKE="${flakeConfig.flakePath}#${flakeConfig.networking.hostName}"
 
-    export PS1="\[\e[38;5;153m\]\u\[\e[0m\]\[\e[38;5;250m\]@\[\e[0m\]\[\e[38;5;33m\]\h\[\e[0m\] \[\e[38;5;250m\]\W\[\e[0m\] ❯ "
+      export PS1="\[\e[38;5;153m\]\u\[\e[0m\]\[\e[38;5;250m\]@\[\e[0m\]\[\e[38;5;33m\]\h\[\e[0m\] \[\e[38;5;250m\]\W\[\e[0m\] ❯ "
 
-    export EDITOR=code
-  '';
+      export EDITOR=code
+    '';
+
+    completion.enable = true;
+  };
 }
