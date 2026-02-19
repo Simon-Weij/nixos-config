987a99b8e1bf6746bbf4a7b4a631cde56b83231f
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-02-19 12:26:02 +0100

    Declarative vscode

--- user/config/bash/bash.nix
+++ user/config/bash/bash.nix
@@ -16,6 +16,8 @@
       export PS1="\[\e[38;5;153m\]\u\[\e[0m\]\[\e[38;5;250m\]@\[\e[0m\]\[\e[38;5;33m\]\h\[\e[0m\] \[\e[38;5;250m\]\W\[\e[0m\] ❯ "
 
       export EDITOR=code
+
+      alias code=codium
     '';
 
     completion.enable = true;
