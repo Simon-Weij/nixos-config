6e395b34af147525df7c790a8a77576c6be77b3e
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-03-01 23:04:31 +0100

    nvim

--- flake.nix
+++ flake.nix
@@ -9,63 +9,80 @@
     fluxer.url = "github:Simon-Weij/Fluxer-nix";
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions/master";
     hjem.url = "github:feel-co/hjem";
+    nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    flatpaks,
-    spicetify-nix,
-    wrappers,
-    hjem,
-    ...
-  }: let
-    system = "x86_64-linux";
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flatpaks,
+      spicetify-nix,
+      wrappers,
+      hjem,
+      nvf,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
 
-    pkgs = import nixpkgs {
-      inherit system;
-    };
-
-    mkSystem = hostName: let
-      hostConfig = import (./hosts + "/${hostName}/meta.nix");
-    in
-      nixpkgs.lib.nixosSystem {
+      pkgs = import nixpkgs {
         inherit system;
-        specialArgs = {
-          inherit inputs;
-          flakeConfig = hostConfig.flakeConfig;
+      };
+
+      mkSystem =
+        hostName:
+        let
+          hostConfig = import (./hosts + "/${hostName}/meta.nix");
+        in
+        nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            flakeConfig = hostConfig.flakeConfig;
+          };
+          modules = [
+            ./system/system.nix
+            ./packages.nix
+            ./user/config/gnome/dconf.nix
+            ./user/config/gnome/extensions.nix
+            ./system/systemd.nix
+            ./user/config/fonts/fonts.nix
+            ./user/config/bash/bash.nix
+            ./user/config/home/home.nix
+            ./user/config/chromium/chromium.nix
+            ./user/config/niri/niri.nix
+            ./user/config/niri/wrappers.nix
+            ./user/config/niri/swww/swww.nix
+            ./user/config/niri/misc/misc.nix
+            (./hosts + "/${hostName}/hardware.nix")
+            (./hosts + "/${hostName}/config.nix")
+            spicetify-nix.nixosModules.spicetify
+            flatpaks.nixosModules.nix-flatpak
+            inputs.hjem.nixosModules.default
+            nvf.nixosModules.default
+          ];
         };
+
+      neovimConfig = nvf.lib.neovimConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
         modules = [
-          ./system/system.nix
-          ./packages.nix
-          ./user/config/gnome/dconf.nix
-          ./user/config/gnome/extensions.nix
-          ./system/systemd.nix
-          ./user/config/fonts/fonts.nix
-          ./user/config/bash/bash.nix
-          ./user/config/home/home.nix
-          ./user/config/chromium/chromium.nix
-          ./user/config/niri/niri.nix
-          ./user/config/niri/wrappers.nix
-          ./user/config/niri/swww/swww.nix
-          (./hosts + "/${hostName}/hardware.nix")
-          (./hosts + "/${hostName}/config.nix")
-          spicetify-nix.nixosModules.spicetify
-          flatpaks.nixosModules.nix-flatpak
-          inputs.hjem.nixosModules.default
+          ./user/config/neovim/neovim.nix
         ];
       };
-  in {
-    nixosConfigurations = {
-      sapphire = mkSystem "sapphire";
-      onyx = mkSystem "onyx";
-    };
+    in
+    {
+      nixosConfigurations = {
+        sapphire = mkSystem "sapphire";
+        onyx = mkSystem "onyx";
+      };
 
-    packages.${system}.vscode = import ./user/config/vscode/vscode.nix {
-      inherit pkgs;
-    };
+      packages.${system} = {
+        neovim = neovimConfig.neovim;
+        default = neovimConfig.neovim;
+      };
 
-    sapphire = self.nixosConfigurations.sapphire;
-    onyx = self.nixosConfigurations.onyx;
-  };
+      sapphire = self.nixosConfigurations.sapphire;
+      onyx = self.nixosConfigurations.onyx;
+    };
 }
---
+++ user/config/neovim/neovim.nix
@@ -0,0 +1,116 @@
+{
+  pkgs,
+  system,
+  ...
+}: {
+  config.vim = {
+    theme = {
+      enable = true;
+      name = "onedark";
+      style = "dark";
+    };
+    lsp.enable = true;
+    languages = {
+      enableTreesitter = true;
+      nix = {
+        enable = true;
+        format = {
+          enable = true;
+          type = ["alejandra"];
+        };
+      };
+      ts.enable = true;
+      go.enable = true;
+      svelte = {
+        enable = true;
+        lsp.enable = true;
+      };
+    };
+    statusline.lualine.enable = true;
+    telescope.enable = true;
+    autocomplete.nvim-cmp.enable = true;
+    visuals.nvim-web-devicons.enable = true;
+
+    git.gitsigns.enable = true;
+
+    filetree.nvimTree = {
+      enable = true;
+      setupOpts = {
+        filters.dotfiles = true;
+        renderer.icons.show.git = false;
+      };
+    };
+
+    formatter.conform-nvim = {
+      enable = true;
+      setupOpts.format_on_save = {
+        lsp_fallback = true;
+        timeout_ms = 500;
+      };
+    };
+
+    globals.mapleader = " ";
+
+    luaConfigRC.nvimtree-git = ''
+      require("nvim-tree").setup({
+        git = {
+          enable = true,
+          ignore = false,
+        },
+        renderer = {
+          highlight_git = "name",
+          icons = {
+            show = {
+              git = false,
+            },
+          },
+        },
+      })
+    '';
+
+    keymaps = [
+      {
+        key = "<leader>e";
+        mode = ["n"];
+        action = ":NvimTreeToggle<CR>";
+        silent = true;
+        desc = "Toggle file tree";
+      }
+      {
+        key = "<C-h>";
+        mode = ["n"];
+        action = "<C-w>h";
+        silent = true;
+        desc = "Move to left window";
+      }
+      {
+        key = "<C-j>";
+        mode = ["n"];
+        action = "<C-w>j";
+        silent = true;
+        desc = "Move to bottom window";
+      }
+      {
+        key = "<C-k>";
+        mode = ["n"];
+        action = "<C-w>k";
+        silent = true;
+        desc = "Move to top window";
+      }
+      {
+        key = "<C-l>";
+        mode = ["n"];
+        action = "<C-w>l";
+        silent = true;
+        desc = "Move to right window";
+      }
+      {
+        key = "<leader>s";
+        mode = ["n"];
+        action = "<cmd>lua vim.lsp.buf.format()<CR><cmd>w<CR>";
+        silent = true;
+        desc = "Format and save";
+      }
+    ];
+  };
+}
