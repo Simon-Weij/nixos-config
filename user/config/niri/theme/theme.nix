d7987309ddfc470b0789dea3202cf7503db4f46f
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-03-05 13:20:42 +0100

    Added darkly qt

--- user/config/niri/theme/theme.nix
+++ user/config/niri/theme/theme.nix
@@ -15,5 +15,17 @@
 
   environment.systemPackages = [
     pkgs.adwaita-icon-theme
+    pkgs.libsForQt5.qtstyleplugin-kvantum
+    pkgs.libsForQt5.qt5ct
+    pkgs.kdePackages.qtstyleplugin-kvantum
+    pkgs.kdePackages.qt6ct
+    pkgs.darkly
+    pkgs.darkly-qt5
   ];
+
+  environment.sessionVariables = {
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+    QT6_QPA_PLATFORMTHEME = "qt6ct";
+    QT_STYLE_OVERRIDE = "kvantum";
+  };
 }
