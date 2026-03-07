7b5211b64b1f2f8cb3bb150891798e5da844f2ae
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-03-07 18:09:54 +0100

    MIT

--- user/config/neovim/neovim.nix
+++ user/config/neovim/neovim.nix
@@ -125,17 +125,6 @@
         })
       '';
 
-      html-indent = ''
-        vim.api.nvim_create_autocmd("FileType", {
-          pattern = "html",
-          callback = function()
-            vim.opt_local.tabstop = 2
-            vim.opt_local.shiftwidth = 2
-            vim.opt_local.expandtab = true
-          end,
-        })
-      '';
-
       terminal-esc = ''
         vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = "Exit terminal mode" })
       '';
