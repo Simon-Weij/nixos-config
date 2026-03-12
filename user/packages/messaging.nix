7a3e4eb0d8354e51f6bb9bcca2fd3c618a736b43
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-03-12 16:47:01 +0100

    Added commet

--- user/packages/messaging.nix
+++ user/packages/messaging.nix
@@ -8,15 +8,24 @@
     inherit pkgs;
     wrappers = inputs.wrappers;
   };
+  commet-hash = "sha256-knMzXnVgzvfYW7NxN8Al8FA8Q+9QLLfQyTrHZ/wLs0c=";
 in {
   users.users."${flakeConfig.username}".packages = [
     inputs.fluxer.packages.${pkgs.stdenv.hostPlatform.system}.default
     vesktopModule.package
   ];
+
   services.flatpak = {
     enable = true;
     packages = [
-      "in.cinny.Cinny"
+      {
+        appId = "com.commet.app";
+        sha256 = commet-hash;
+        bundle = "${pkgs.fetchurl {
+          url = "https://github.com/commetchat/commet/releases/download/v0.4.1/chat.commet.commetapp.flatpak";
+          sha256 = commet-hash;
+        }}";
+      }
     ];
   };
 }
