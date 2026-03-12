e6ba7bf9238f941cfb47870e9899b37f0eef1685
Simon <simongvanderweij@gmail.com>
2026-03-12 15:26:45 +0100

    Reinstalled onyx

--- hosts/onyx/meta.nix
+++ hosts/onyx/meta.nix
@@ -1,7 +1,7 @@
 {
   flakeConfig = {
     flakePath = "/home/simon/Documents/nixos-config";
-    stateVersion = "25.05";
+    stateVersion = "25.11";
     username = "simon";
     networking.hostName = "onyx";
     hashedPassword = "$y$j9T$Lh5xfMWjdQPXq1SY5h1if.$H04U9/Kfl1YHR7shomYVpltWWjx/oNR4PCtuLQ8IxRA";
