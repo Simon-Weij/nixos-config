cc329e214fc4f1113a58cc6e2f3abb1774403d90
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-03-25 21:54:25 +0100

    Lanzaboote peak

--- modules/nixosModules/system/bootloader.nix
+++ modules/nixosModules/system/bootloader.nix
@@ -1,6 +1,25 @@
 {inputs, ...}: {
-  flake.nixosModules.bootloader = {pkgs, ...}: {
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
+  flake.nixosModules.bootloader = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    boot = {
+      bootspec.enable = true;
+      lanzaboote = {
+        enable = true;
+        autoGenerateKeys.enable = true;
+        autoEnrollKeys = {
+          enable = true;
+          autoReboot = true;
+        };
+        pkiBundle = "/etc/secureboot";
+      };
+      loader.systemd-boot.enable = lib.mkForce false;
+      loader.grub.enable = lib.mkForce false;
+    };
+    environment.systemPackages = [
+      pkgs.sbctl
+    ];
   };
 }
