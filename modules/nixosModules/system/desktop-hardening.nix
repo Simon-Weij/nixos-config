f0948e931b22ca0126bb107d70658202858d7da2
Simon <simongvanderweij@gmail.com>
2026-03-31 21:36:40 +0200

    Idk

--- modules/nixosModules/system/desktop-hardening.nix
+++ modules/nixosModules/system/desktop-hardening.nix
@@ -6,7 +6,7 @@
   flake.nixosModules.desktop-hardening = {pkgs, ...}: {
     networking.firewall.enable = true;
     services.gnome.gnome-keyring.enable = true;
-    security.pam.services.niri.enableGnomeKeyring = true;
+    security.pam.services.login.enableGnomeKeyring = true;
 
     xdg.portal = {
       enable = true;
