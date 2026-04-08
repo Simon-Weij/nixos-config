3c5e19ceb6a8c1a04593e0971aa5cb95b090e9fd
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-04-08 11:38:57 +0200

    Updated settings

--- modules/nixosModules/system/desktop-hardening.nix
+++ modules/nixosModules/system/desktop-hardening.nix
@@ -8,12 +8,8 @@
     services.gnome.gnome-keyring.enable = true;
     security.pam.services.login.enableGnomeKeyring = true;
 
-    xdg.portal = {
-      enable = true;
-      extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
-      ];
-      config.common.default = "*";
-    };
+    environment.systemPackages = [
+      pkgs.seahorse
+    ];
   };
 }
