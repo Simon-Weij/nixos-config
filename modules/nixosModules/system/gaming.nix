dbfe458b012db538e3a3a700df91ca4b0c91860a
Simon <175155691+Simon-Weij@users.noreply.github.com>
2026-04-15 16:38:32 +0200

    Disko

--- modules/nixosModules/system/gaming.nix
+++ modules/nixosModules/system/gaming.nix
@@ -4,9 +4,7 @@
     lib,
     flakeConfig,
     ...
-  }: let
-    #    hytale-hash = "sha256-uu7FA36M4wSqDXRF1ffN1a8S5MjL1N1kZi4gwbpw1oY=";
-  in {
+  }: {
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true;
