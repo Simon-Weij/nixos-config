dde22344984f4bdceffe2e5072761884113d6481
Simon <simongvanderweij@gmail.com>
2026-06-18 13:45:39 +0000

    Added ruby

---
+++ modules/hosts/ruby/ruby.nix
@@ -0,0 +1,27 @@
+{
+  inputs,
+  self,
+  ...
+}: let
+  flakeConfig = {
+    flakePath = "/home/simon/Documents/nixos-config";
+    stateVersion = "26.05";
+    username = "simon";
+    networking.hostName = "ruby";
+    isLaptop = true;
+  };
+  modules = self.nixosModules;
+in {
+  flake.nixosConfigurations.ruby = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = {inherit flakeConfig inputs self;};
+    modules = with modules; [
+      ./hardware.nix
+      ./disko.nix
+      imports
+      {
+        networking.networkmanager.enable = true;
+      }
+      system
+    ];
+  };
+}
